108be1a267225b4316fbafbf25e3218e7ce864ca
christopherbar <bartlett@pdx.edu>
2017-07-06 20:16:10 -0700

    added definitions for all the possible note transitions

--- note.py
+++ note.py
@@ -1,10 +1,103 @@
 
+
+# ------------------------------------------------------------------------------
+# Packages 
+# ------------------------------------------------------------------------------
+
+
 import copy
 
 
+# ------------------------------------------------------------------------------
+# Global Variables
+# ------------------------------------------------------------------------------
+
+
+## this is the verbosity variable, set
+## it to true when you need information.
 VERBOSE = True 
 
+
 # ------------------------------------------------------------------------------
+
+
+## derived from 
+##   https://en.wikipedia.org/wiki/Consonance_and_dissonance#Consonance
+
+## these are lists of all the perfect and imperfect consonances 
+allConsonances = {
+  'perfect'   : ['unisons', 'octaves', 'fourths', 'fifths'],
+  'imperfect' : ['majorThirds', 'minorSixths', 'minorThirds', 'majorSixths'],
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+## derived from
+##  http://www2.siba.fi/muste1/index.php?id=65&la=en
+
+## these are lists of all the hard and soft dissonances. 
+allDissonances = {
+  'sharp' : ['minorSecond', 'majorSeventh'],
+  'soft'  : ['majorSeconds', 'minorSeventh', 'tritone'],
+}
+
+
+# ------------------------------------------------------------------------------
+
+
+## derived from the list of links at 
+##   https://en.wikipedia.org/wiki/Consonance_and_dissonance#Consonance
+
+## this dictionary contains all of the ways to reach notes either 
+## via semitones or via frequencies, as im not sure which i will be using.
+allTransitions = {
+  # https://en.wikipedia.org/wiki/Unison
+  'unison'      : [[0], [[1/1]]],            ## +00 semis
+
+  # https://en.wikipedia.org/wiki/Major_second
+  'minorSecond' : [[1], [[16/15], [17/16]]], ## +01 semis 27/25, 135/128, 25/24
+
+  # https://en.wikipedia.org/wiki/Semitone
+  'majorSecond' : [[2], [[9/8], [10/9]]],    ## +02 semis
+
+  # https://en.wikipedia.org/wiki/Minor_third
+  'minorThird'  : [[3], [[6/5], [19/16]]],   ## +03 semis
+
+  # https://en.wikipedia.org/wiki/Major_third
+  'majorThird'  : [[4], [[5/4]]],            ## +04 semis
+
+  # https://en.wikipedia.org/wiki/Perfect_fourth
+  'fourth'      : [[5], [[4/3]]],            ## +05 semis
+
+  # https://en.wikipedia.org/wiki/Tritone
+  'tritone'     : [[6], [[25/18], [36/25]]], ## +06 semis 45/32, 64/45, 7/5,...
+
+  # https://en.wikipedia.org/wiki/Perfect_fifth
+  'fifth'       : [[7], [[3/2]]],            ## +07 semis
+
+  # https://en.wikipedia.org/wiki/Minor_sixth
+  'minorSixth'  : [[8], [[8/5], [11/7]]],    ## +08 semis
+
+  # https://en.wikipedia.org/wiki/Major_sixth
+  'majorSixth'  : [[9], [[5/3], [12/7]]],    ## +09 semis
+
+  # https://en.wikipedia.org/wiki/Minor_seventh
+  'minorSeventh': [[10], [[16/9], [9/5]]],   ## +10 semis
+
+  # https://en.wikipedia.org/wiki/Major_seventh
+  'majorSeventh': [[11], [[15/8], [50/27]]], ## +11 semis
+
+  # https://en.wikipedia.org/wiki/Octave
+  'octave'      : [[12], [[2/1]]],           ## +12 semis
+}
+
+
+# ------------------------------------------------------------------------------
+# Functions
+# ------------------------------------------------------------------------------
+
 
 # defines the base printing function for this file,
 # if the VERBOSE variable is true, it will print things 
@@ -13,7 +106,9 @@ def vprint(string):
   if VERBOSE :
     print (string)
 
+
 # ------------------------------------------------------------------------------
+
 
 # defines the function that is used to populate the list 
 # of all possible notes, and their definitions.  This includes 
@@ -25,46 +120,50 @@ def getNotes():
 
   vprint("getting all possible notes.")
 
+  ## loop for all notes from A to G, all the ones in the scale.
   for note in ('A', 'B', 'C', 'D', 'E', 'F', 'G'):
 
     ## add and print the note
     possNotes += [note]
-    vprint (str('adding') + note)
+    vprint (str('adding ') + note)
 
     ## the notes that dont have sharps are b, and e.
     if (not (note == 'B' or note == 'E')):
       tempNote = note + '#'
       possNotes += [tempNote]
-      vprint(str('adding') + tempNote)
+      vprint(str('adding ') + tempNote)
 
-  ## pNotes now stores a list of all possible note names
-
+  ## possNotes now stores a list of all possible note names
+  ## now we need to add in functions to get different notes
   allNotes = []
 
-  return possNotes
+  for note in possNotes :
+    vprint (note)
+
+
+  ## return allNotes which now has all notes 
+  ## we can reach from a given note.
+  return allNotes 
+
 
 # ------------------------------------------------------------------------------
+# Execution
+# ------------------------------------------------------------------------------
+
 
 chain = ""
-
 newLink  = ""
 
-tempNote = None
-
+# call the getNotes function to populate all possible notes
 possibleNotes = getNotes()
 
-vprint(possibleNotes)
+# did we get anything in the notes section
+vprint(str("possibleNotes: ") + str(possibleNotes))
+
+# how to get into the transitions
+vprint (allTransitions["octave"])
 
 
-musicNote = {
-   'name' : 'unInit',
-   'fourths' : [],
-   'fifths' : [],
-   'octaves' : [],
-   'minorThird' : [],
-   'majorThird' : [],
-}
-
-vprint(musicNote["name"])
-
-
+# ------------------------------------------------------------------------------
+# End
+# ------------------------------------------------------------------------------
