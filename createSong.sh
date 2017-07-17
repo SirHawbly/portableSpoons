34d7073ca32b3214e6c7614921140a7b3a5c7a63
christopherBar <SirHawbly@users.noreply.github.com>
2017-07-17 01:53:51 -0700

    long overdue push, added hotcross bunssssss

--- createSong.sh
+++ createSong.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+"""
+  * Copyright (c) 2017 Christopher Bartlett
+  * [This program is licensed under the "GPL License"]
+  * Please see the file LICENSE in the source
+  * distribution of this software for license terms.
+"""
+
+
 python makeSong.py $1.mid
 
 timidity $1.mid
